a36282282ae37f3085d4369207c04e3bb7595efa
Tom Danvers <tdanvers@apple.com>
2023-01-14 21:30:55 +0000

    Code for pi

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 var glow = require('piglow');
 
-var PINS = [
-    'l_0_0',
-    'l_0_1',
-    'l_0_2',
-    'l_0_3',
-    'l_0_4',
-    'l_0_5',
-    'l_1_0',
-    'l_1_1',
-    'l_1_2',
-    'l_1_3',
-    'l_1_4',
-    'l_1_5',
-    'l_2_0',
-    'l_2_1',
-    'l_2_2',
-    'l_2_3',
-    'l_2_4',
-    'l_2_5'
-];
+var lookupTable = {
+    l_0_0: 0, l_0_1: 1, l_0_2: 2, l_0_3: 3, l_0_4: 14, l_0_5: 12,
+    l_1_0: 6, l_1_1: 7, l_1_2: 8, l_1_3: 5, l_1_4: 4, l_1_5: 9,
+    l_2_0: 17, l_2_1: 16, l_2_2: 15, l_2_3: 13, l_2_4: 11, l_2_5: 10
+};
 
+
+
+var PINS = [];
 var PIN_MAP = {};
 
-for (var i = 0; i < PINS.length; i ++) {
-    PIN_MAP[String(i+1)] = PINS[i];
+for (var key in  lookupTable) {
+    PINS[lookupTable[key]] = key;
+    PIN_MAP[lookupTable[key] + 1] = key;
 }
 
-// var ENABLED = [1,5,10,15];
+console.log(PINS);
+console.log(PINS_MAP);
+
  
 glow((error, pi) => {
     if (error) {
         console.log(error);
     } else {
-        
-        
-
         var index = 0;
 
         setInterval(function() {
